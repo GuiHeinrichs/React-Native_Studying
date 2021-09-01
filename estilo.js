095f8e0be5524615df68101ceecad2c6b676d3b2
João Guilherme Heinrichs <63328794+GuiHeinrichs@users.noreply.github.com>
2021-09-01 17:15:34 -0300

    Add files via upload

---
+++ estilo.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+    txtG:{
+        fontSize: 50,
+        textAlign: "center"
+        
+    },
+    txtM:{
+        fontSize: 30,
+        textAlign: "center"
+    }
+})
